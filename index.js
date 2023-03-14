7c6a29ab3016bf064b93f06f46e75aba521bb455
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-03-14 21:01:03 +0200

    homework-01

---
+++ index.js
@@ -0,0 +1,55 @@
+// "use strikt"
+
+/////////TASK-1 (ПЕРЕМЕННЫЕ)//////////////
+
+// const userName = "Вася";
+
+// const user = userName;
+
+// console.log(user)
+
+
+////////TASK-2 (ТИПЫ ДАННЫХ)///////////////
+
+// Вариант-1
+
+// let userAge = 36;
+// let userInfo = `Фрилансер ${userAge}`;
+// console.log(userInfo)
+
+// Вариант-2
+
+// let userHeight = 145 / 0;
+// console.log(userHeight)
+// // вернется значение Infinity
+
+// Вариант-3
+
+// let userName;
+// console.log(typeof userName);
+// // вернется значение Undefined
+
+// Вариант-4
+
+// let userSize = "45" / "8";
+// console.log(typeof userSize)
+// // вернет значение Number
+
+
+///////// TASK-03 (операторы) //////////
+
+// console.log('35' + '22'); //// 3522
+// console.log('35' * '22'); //// 770
+// console.log('558' > 22++);  //// ошибка
+
+
+// let userCounter = 0;
+// let newUser = userCounter++;
+// console.log(newUser);
+////  вернет 0
+
+
+// console.log(!false && 11 || 18 && !''); // вернет 11
+
+let name = 0;
+console.log(name ?? "без имени")
